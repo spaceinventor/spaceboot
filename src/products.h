e351e081df90b61e21f554c5f1c1402dbfe8c2b9
Johan De Claville Christiansen <johan@safeex.com>
2017-04-06 11:51:46 +0200

    Upgrade e70 to miniboot

--- src/products.h
+++ src/products.h
@@ -14,7 +14,7 @@ static const struct {
 	unsigned int flash0_addr;
 	unsigned int flash1_addr;
 } products[] = {
-	{"e70", "e70", 0x400000, 0x480000},
+	{"e70", "e70", 0x404000, 0x480000},
 	{"pdu", "pdu", 0x4000, 0x22000},
 	{"mppt", "mppt", 0x4000, 0x22000},
 	{"dise", "dise", 0x4000, 0x22000},
